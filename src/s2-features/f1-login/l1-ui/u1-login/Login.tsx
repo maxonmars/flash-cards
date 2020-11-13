16f42d53a2cd52aff8ef90c2e1c12207f5a91daf
Maxim <turbo3.6@cn.ru>
2020-11-14 00:56:05 +0700

    max first

--- src/s2-features/f1-login/l1-ui/u1-login/Login.tsx
+++ src/s2-features/f1-login/l1-ui/u1-login/Login.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
+import { SuperInputText } from '../../../../s1-main/m1-ui/u0-common/c1-SuperInputText/SuperInputText'
+import { SuperButton } from '../../../../s1-main/m1-ui/u0-common/c2-SuperButton/SuperButton'
 
 export const Login = () => {
-   return <div>Login</div>
+   return (
+      <div>
+         <SuperInputText />
+         <SuperInputText />
+         <SuperInputText />
+         <SuperButton />
+      </div>
+   )
 }
