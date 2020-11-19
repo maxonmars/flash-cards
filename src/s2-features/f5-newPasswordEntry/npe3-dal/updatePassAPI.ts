80a6b98902968b655a601c947a4c6515a80c6731
Анатолий Митрошин <mymailtolaaa@gmail.com>
2020-11-19 20:49:01 +0300

    add changes to push data

--- src/s2-features/f5-newPasswordEntry/npe3-dal/updatePassAPI.ts
+++ src/s2-features/f5-newPasswordEntry/npe3-dal/updatePassAPI.ts
@@ -1,7 +1,7 @@
 import { instance } from '../../../s1-main/m3-dal/instance'
 
 export const updatePassAPI = {
-   updatePass: (email: string, resetToken: string) => {
-      return instance.post('/auth/set-new-password', { email, resetToken })
+   updatePass: (password: string, resetPasswordToken: string) => {
+      return instance.post('/auth/set-new-password', { password, resetPasswordToken })
    },
 }
