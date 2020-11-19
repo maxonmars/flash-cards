0280451e3b02246108ac03f9646bcc6aacff0eac
Maxim <turbo3.6@cn.ru>
2020-11-19 23:58:53 +0700

    Release v0.1.3

--- src/s2-features/f4-passwordRecovery/pr3-dal/passwordRecoveryAPI.ts
+++ src/s2-features/f4-passwordRecovery/pr3-dal/passwordRecoveryAPI.ts
@@ -4,7 +4,7 @@ const messageView = (token: string) => {
    return `<div style="background-color: #00ff00; padding: 15px"><a href=http://localhost:3000/#/set-new-password/${token}'>Ссылка на восстановление пароля</a></div>`
 }
 const messageMelivaro = (token: string) => {
-   return `<div style="background-color: #00ff00; padding: 15px"><a href=https://melivaro.github.io/#/set-new-password/${token}'>Ссылка на восстановление пароля</a></div>`
+   return `<div style="background-color: #00ff00; padding: 15px"><a href=https://melivaro.github.io/flash-cards/#/set-new-password/${token}'>Ссылка на восстановление пароля</a></div>`
 }
 
 export const passRecoveryAPI = {
