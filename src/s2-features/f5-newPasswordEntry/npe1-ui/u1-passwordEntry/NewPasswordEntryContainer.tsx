a0974e1adb25c06385eebaa27a72a7e8683ecbcb
Анатолий Митрошин <mymailtolaaa@gmail.com>
2020-11-20 17:09:29 +0300

    Fix forgot dataRequest

--- src/s2-features/f5-newPasswordEntry/npe1-ui/u1-passwordEntry/NewPasswordEntryContainer.tsx
+++ src/s2-features/f5-newPasswordEntry/npe1-ui/u1-passwordEntry/NewPasswordEntryContainer.tsx
@@ -36,17 +36,16 @@ export const NewPasswordEntryContainer = () => {
          if (!confirmPassword) {
             errors.password = 'Password is required'
          }
-         if (password.length < 3) {
-            errors.password = 'Пароль не может быть меньше 3х символов'
+         if (password.length < 8) {
+            errors.password = 'Пароль не может быть меньше восьми символов'
          }
-         if (confirmPassword.length < 3) {
-            errors.confirmPassword = 'Пароль не может быть меньше 3х символов'
+         if (confirmPassword.length < 8) {
+            errors.confirmPassword = 'Пароль не может быть меньше восьми символов'
          }
          return errors
       },
       onSubmit: (values) => {
          const { password, confirmPassword } = values
-
          if (password === confirmPassword) {
             dispatch(updatePasswordTC.updatePass(password, token))
          } else {
