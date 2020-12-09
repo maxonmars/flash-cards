4ab0e750917e6e54d7be6727901b98468f6e6e06
Maxim <turbo3.6@cn.ru>
2020-12-09 23:54:10 +0700

    add SuperMultiplePreview.tsx

--- src/s1-main/m1-ui/u0-common/ComponentsTestRack.tsx
+++ src/s1-main/m1-ui/u0-common/ComponentsTestRack.tsx
@@ -4,6 +4,7 @@ import { SuperButton } from './c2-SuperButton/SuperButton'
 import { SuperCheckbox } from './c3-SuperCheckbox/SuperCheckbox'
 import { FileInput } from '../../../s3-dev/d1-FileInput/FileInput'
 import { SuperInputFiles } from '../../../s3-dev/d2-SuperFileInput/SuperInputFiles'
+import { SuperMultiplePreview } from '../../../s3-dev/d3-SuperMultiplePreview/SuperMultiplePreview'
 
 export const ComponentsTestRack = () => {
    return (
@@ -14,9 +15,10 @@ export const ComponentsTestRack = () => {
          {/*<SuperInputText placeholder={'Password'}/>*/}
          {/*<SuperButton children={'Log in'}/>*/}
          {/*<SuperCheckbox children={'Remember Me'}/>*/}
-         <FileInput value={'aaaa'} />
-         <hr />
-         <SuperInputFiles />
+         {/*<FileInput value={'aaaa'} />*/}
+         {/*<hr />*/}
+         {/*<SuperInputFiles />*/}
+         <SuperMultiplePreview />
       </div>
    )
 }
